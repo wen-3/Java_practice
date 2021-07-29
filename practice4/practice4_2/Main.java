40caa6174f4712a7fe610f4530c9f3401ef7dcd7
wen-3 <sd63324@gmail.com>
2021-07-29 23:26:15 +0800

    upload

--- practice4/practice4_2/Main.java
+++ practice4/practice4_2/Main.java
@@ -3,7 +3,7 @@ package practice4.practice4_2;
 import java.util.Scanner;
 
 // 變差數列
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -14,6 +14,21 @@ public class Main {
             arr[i] = num;
         }
 
-        // 判斷
+        int differenceArr[] = new int[n - 1];
+        for (int i = 1; i < arr.length; i++){
+            int num = arr[i] - arr[i - 1];
+            differenceArr[i - 1] = num;
+        } 
+
+        int r = differenceArr[1] - differenceArr[0];
+        boolean result = true;
+        for (int i = 2; i < differenceArr.length; i++){
+            if (differenceArr[i] - differenceArr[i - 1] != r){
+                result = false;
+                break;
+            }
+        }
+
+        System.out.println(result);
     }
 }
