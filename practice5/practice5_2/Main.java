5becaf408f472613d97f73d11324a5a2963ea876
wen-3 <sd63324@gmail.com>
2021-07-30 16:18:13 +0800

    upload
    補充註解

--- practice5/practice5_2/Main.java
+++ practice5/practice5_2/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[][] = new int[10][2];
+        // 補充
+        // arr[][0]代表學生座號；arr[][1]代表學生缺席次數
 
         // 學生座號
         for (int i = 0; i < arr.length; i++){
@@ -17,12 +19,14 @@ public class Main {
         // 學生缺席次數
         // 輸入五天的缺席名單
         for (int i = 0; i < 5; i++){
-            int count = sc.nextInt();
+            int count = sc.nextInt();   // 缺席人數
             if (count == -1){
                 continue;
             }
             for (int j = 0; j < count; j++){
-                int n = sc.nextInt();
+                int n = sc.nextInt();   // 缺席的座號
+
+                // 計算n座號缺席的次數
                 for (int k = 0; k < arr.length; k++){
                     if (n == arr[k][0])
                     arr[k][1]++;
