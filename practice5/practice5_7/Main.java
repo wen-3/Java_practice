1e54d9fb8ef88e589169e8ddf7a57e39480a5aa5
wen-3 <sd63324@gmail.com>
2021-07-30 14:22:10 +0800

    upload
    修正細節

--- practice5/practice5_7/Main.java
+++ practice5/practice5_7/Main.java
@@ -22,17 +22,29 @@ public class Main {
         // 判斷三人輸贏
         int max = 0;
         int win = 0;
+        int count = 0;   // 最大值出現次數
         for (int i = 0; i < arr.length; i++){   // 三個人
             if (max < arr[i]){
                 max = arr[i];
                 win = i;
-            } else if (max == arr[i]){
-                System.out.println("不分勝負");
-                break;
             }
 
+            int key = max;   // 確認最終最大值出現幾次
+
             if (i == arr.length -1){
-                switch (win) {
+                for (int value : arr){
+                    if (value == key){
+                        count++;     // 最終最大值出現次數
+                    }
+                }
+
+                if (count > 1){    // 代表擲出最大值數值不只一人
+                    System.out.println("不分勝負");
+                    break;
+                }
+
+                // 執行到此行，代表擲出最大值的只有一人
+                switch (win) {    // 判斷擲出最大值的人是誰
                     case 0:
                         System.out.println("A");
                         break;
